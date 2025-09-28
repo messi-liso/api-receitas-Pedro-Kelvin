84990c7074f2ab31cee319b1c41e5ac323ff4565
Jp <joao.pedro47@aluno.ifce.edu.br>
2025-09-28 21:02:12 +0000

    alterações

--- main.py
+++ main.py
@@ -91,6 +91,10 @@ def criar_receita(dados: CreateReceita):
 
 @app.put("/receitas/{id}")
 def update_receita(id: int, dados: CreateReceita):
+    for Receita in receitas:
+        if Receita.id != id and Receita.nome == dados.nome:
+            return {"mensagem": "receita com esse nome já existe"}
+        
     for i in range(len(receitas)):
         if receitas[i].id == id:
             receita_atualizada = Receita(
