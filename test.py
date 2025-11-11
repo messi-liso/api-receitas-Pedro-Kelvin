5fc0c0cbb615f49a8d7117013fc274fc1006d3d4
Jp <joao.pedro47@aluno.ifce.edu.br>
2025-11-11 12:10:28 +0000

    nova atividade

---
+++ test.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from models import User, table_registry
+
+app = FastAPI(title='API de teste')
+
+
+engine = create_engine("sqlite:///:memory:", echo=False)
+
+
+table_registry.metadata.create_all(engine)
+
+with Session(engine) as session:
+    mairon = User(
+        username="mairon", password="senha123", email="mairon@email.com"
+    )
+    session.add(mairon)
+    session.commit()
+    session.refresh(mairon)
+    
+print("DADOS DO USUARIO:", mairon)
+print("ID:", mairon.id)
+print("Criado em:", mairon.created_at)
